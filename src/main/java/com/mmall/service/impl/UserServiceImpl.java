cc84c9f3c0d2eef7bcb7228193673c130cb0396a
pinghengxing <a454902046@163.com>
2018-08-10 18:26:32 +0800

    'd'

---
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.mmall.service.impl;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.dao.UserMapper;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserServiceImpl implements IUserService {
+    @Autowired
+    private UserMapper userMapper;
+    @Override
+    public ServerResponse<User> login(String username, String password) {
+        int resultCount = userMapper.checkUsername(username);
+        if(resultCount == 0){
+            return ServerResponse.createByErrorMessage("用户名不存在");
+        }
+        //todo  md5密码登录
+        return null;
+    }
+}
